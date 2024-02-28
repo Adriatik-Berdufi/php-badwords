90c3514bbe0df923fdc13109ec57ff17f1769f78
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-02-28 09:01:49 +0100

    add php

--- result.php
+++ result.php
@@ -1,4 +1,8 @@
 <?php 
+$user_input = $_GET['inputText'];
+$input_size = strlen(trim($user_input));
+$bad_word = $_GET[trim('badWord')];
+$word_censured = str_replace($bad_word, '*****', $user_input);
 
 ?>
 <!DOCTYPE html>
@@ -11,6 +15,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+<?php echo $user_input ?>
+<br>
+<?php echo $input_size ?>
+<br>
+<?php echo $bad_word ?>
+<br>
+<?php echo $word_censured ?>
     
+        
+   
 </body>
 </html>
